2863815695dde93e6956c9360bb3a0ab0b64c8c2
kawashiro-ryofu <unsciencific_kappa@foxmail.com>
2020-07-10 19:47:26 +0800

    DEV02.4

--- Linux/mGenerate.py
+++ Linux/mGenerate.py
@@ -2,62 +2,108 @@
 
 #
 #   Mashiro (Linux ver.)
-#   Version:    0.00 DEV(XFCE)
+#   Version:    0.02 DEV(XFCE)
 #   
 #   (C)Copyright 2020 RYOUN & the Mashiro Developers
 #
 #   mGenerate.pyw: WordCloud Background Maker
 #
+
+import tkinter.messagebox as tkm
 import wordcloud as wc
 import matplotlib.pyplot as plt
 import time
 import traceback
 import json
+import signal
+
+#Exit Function
+def offsig(signum,frame):
+    exit(signum)
+
+#Bind 
+signal.signal(signal.SIGTSTP, offsig)
 
 class SETTINGS:
+    #Default Settings
+    
+    #Color  Element 1:Daylight Mode;
+    #       Element 2:Background Color.
     Color = [1,"white"]
+    #Margin Of The Word Cloud
     Margin = 0
+    #Word Font
     Font = "/usr/share/fonts/opentype/noto/NotoSerifCJK-Medium.ttc"
+    #Screen Resolution
     Resolution = [1024,768]
+    #Word Cloud Mask Image
+    #Disable:0
     Mask = "0"
+    #Default Text Source
     TextSource = "./WCT.txt"
-    AutoRefresh = 0
+    #Autorefresh Inervals(minutes)
+    AutoRefresh = 1
+    #Wallpaper Image Output Source
+    Output = "~/.mashiro"
+
     def __init__(self):
         try:
             profile = json.loads(open("./settings.json","r").read())
         except:
-            traceback.format_exc()
+            tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
 
-        self.Color[0] = profile["Settings"]["BG-Color"]["Daylight"]
-        self.Color[1] = profile["Settings"]["BG-Color"]["Color"]
-        self.Margin = profile["Settings"]["BG-Margin"]
-        self.Font = profile["Settings"]["BG-Font"]
-        self.Resolution[0] = profile["Settings"]["Resolution"]['x']
-        self.Resolution[1] = profile["Settings"]["Resolution"]['y']
-        self.Mask = profile["Settings"]["Mask"]
-        self.TextSource = profile["Settings"]["TextSource"]
-        self.AutoRefresh = profile["Settings"]["AutoRefreshInterval"]
-        
+        try:
+            self.Color[0] = profile["Settings"]["BG-Color"]["Daylight"]
+            self.Color[1] = profile["Settings"]["BG-Color"]["Color"]
+            self.Margin = profile["Settings"]["BG-Margin"]
+            self.Font = profile["Settings"]["BG-Font"]
+            self.Resolution[0] = profile["Settings"]["Resolution"]['x']
+            self.Resolution[1] = profile["Settings"]["Resolution"]['y']
+            self.Mask = profile["Settings"]["Mask"]
+            self.TextSource = profile["Settings"]["TextSource"]
+            self.AutoRefresh = profile["Settings"]["AutoRefreshInterval"]
+            self.Output = profile["Settings"]["Output"]
+        except:
+            tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
 
 def main():
 
     while 1:
+        setting = SETTINGS()
+        try:
+            # WCT.txt:Words To Generate Background Wallpaper
+            words = open(setting.TextSource,'r').read()
+            now = time.localtime(time.time())
+        except:
+            tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
 
-        words = open("./WCT.txt",'r').read()
-        now = time.localtime(time.time())
+        try:
+            # Daylight Background Color
+            if(setting.Color[0] == 1):
+                if(now.tm_hour > 6 | now.tm_hour < 19):
+                    color = "white"
+                else:
+                    color = "black"
+            else:
+                color = setting.Color[1]
+        except:
+            tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
+        try:
+        
+            # Generate Wordcolud
+            front = wc.WordCloud(background_color=color,font_path=setting.Font ,width = setting.Resolution[0],height = setting.Resolution[1],margin = setting.Margin).generate(words)
 
-        if(now.tm_hour > 6 | now.tm_hour < 19):
-            color = "white"
-        else:
-            color = "black"
+            plt.imshow(front)
+            front.to_file(setting.Output)
+        except:
+            tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
 
-        front = wc.WordCloud(background_color=color,font_path='/usr/share/fonts/opentype/urw-base35/NimbusSans-Bold.otf' ,width = 1440,height = 900,margin = 3).generate(words)
-
-        plt.imshow(front)
-        plt.axis('off')
-        front.to_file("./a.jpg")
-
-        time.sleep(60)
+        time.sleep(setting.AutoRefresh * 60)
 
 
 
--- Win32/mGenerate.pyw
+++ Win32/mGenerate.pyw
@@ -2,7 +2,7 @@
 
 #
 #   Mashiro (Win32 ver.)
-#   Version:    0.00 DEV
+#   Version:    0.02 DEV
 #   
 #   (C)Copyright 2020 RYOUN & the Mashiro Developers
 #
@@ -17,31 +17,54 @@ import win32api, win32gui, win32con,os
 import traceback
 import json
 
+#Exit Function
+def offsig(signum,frame):
+    exit(signum)
+
+#Bind 
+signal.signal(signal.SIGTSTP, offsig)
+
+
 class SETTINGS:
+    #Default Settings
+    
+    #Color  Element 1:Daylight Mode;
+    #       Element 2:Background Color.
     Color = [1,"white"]
+    #Margin Of The Word Cloud
     Margin = 0
-    Font = "C:\\Windows\\Fonts\\ARIALN.TTF"
+    #Word Font
+    Font = "/usr/share/fonts/opentype/noto/NotoSerifCJK-Medium.ttc"
+    #Screen Resolution
     Resolution = [1024,768]
+    #Word Cloud Mask Image
+    #Disable:0
     Mask = "0"
+    #Default Text Source
     TextSource = "./WCT.txt"
-    AutoRefresh = 0
+    #Autorefresh Inervals(minutes)
+    AutoRefresh = 1
     def __init__(self):
         
         try:
             profile = json.loads(open("./settings.json","r").read())
         except:
             tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
+        try:
+            self.Color[0] = profile["Settings"]["BG-Color"]["Daylight"]
+            self.Color[1] = profile["Settings"]["BG-Color"]["Color"]
+            self.Margin = profile["Settings"]["BG-Margin"]
+            self.Font = profile["Settings"]["BG-Font"]
+            self.Resolution[0] = profile["Settings"]["Resolution"]['x']
+            self.Resolution[1] = profile["Settings"]["Resolution"]['y']
+            self.Mask = profile["Settings"]["Mask"]
+            self.TextSource = profile["Settings"]["TextSource"]
+            self.AutoRefresh = profile["Settings"]["AutoRefreshInterval"]
+        except:
+            tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
 
-        self.Color[0] = profile["Settings"]["BG-Color"]["Daylight"]
-        self.Color[1] = profile["Settings"]["BG-Color"]["Color"]
-        self.Margin = profile["Settings"]["BG-Margin"]
-        self.Font = profile["Settings"]["BG-Font"]
-        self.Resolution[0] = profile["Settings"]["Resolution"]['x']
-        self.Resolution[1] = profile["Settings"]["Resolution"]['y']
-        self.Mask = profile["Settings"]["Mask"]
-        self.TextSource = profile["Settings"]["TextSource"]
-        self.AutoRefresh = profile["Settings"]["AutoRefreshInterval"]
-        
 def applyBG(pic):
     # Apply Background Wallpaper 
     # * https://www.jb51.net/article/155070.htm
@@ -64,6 +87,7 @@ def main():
         except:
             # ERROR OUTPUT
             tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
 
         try:
             # Daylight Background Color
@@ -83,6 +107,7 @@ def main():
             
         except:
             tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
             
         try:
             #Apply
@@ -90,6 +115,7 @@ def main():
        
         except:
             tkm.showerror("Error",traceback.format_exc())
+            offsig(0,0)
 
 
         #(Wait)
